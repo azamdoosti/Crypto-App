d074d2901572ee0bf2d42fc3da6b8bd4ade4ec96
azam <a.doosti@gmail.com>
2025-04-08 13:36:57 +0330

    Adding Chart

--- src/components/Modules/Chart.jsx
+++ src/components/Modules/Chart.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
 import styles from './Chart.module.css'
 import convertData from '../../helpers/ConvertData'
-import { CartesianAxis, CartesianGrid } from 'recharts'
+import { CartesianGrid , ResponsiveContainer , LineChart, YAxis, XAxis, Tooltip , Line, Legend } from 'recharts'
 
 function Chart({chart , setChart}) {
   const [type,setType]=useState("prices")
@@ -12,11 +13,7 @@ function Chart({chart , setChart}) {
      <span className={styles.cross} onClick={()=> setChart(null)}>X</span>
      <div className={styles.chart}>
       <div className={styles.graph}>
-        <ResponsiveContainer width='100%' height='100%'>
-          <LineChart width={400} height={400} data={convertData(type, chart)}>
-            <CartesianGrid stroke='#404042' />
-          </LineChart>
-        </ResponsiveContainer>
+        <ChartComponent data={convertData(chart,type)} type={type}/>
       </div>
      </div>
     </div>
@@ -24,3 +21,18 @@ function Chart({chart , setChart}) {
 }
 
 export default Chart
+
+const ChartComponent = ({data , type}) => {
+return (
+  <ResponsiveContainer width='100%' height='100%'>
+  <LineChart width={400} height={400} data={data}>
+    <Line type="monotone" dataKey={type} stroke='#3874ff' strokeWidth="2px"/>
+    <CartesianGrid stroke='#404042' />
+    <YAxis dataKey={type} domain={["auto" , "auto"]}/>
+    <XAxis dataKey="date" hide/>
+    <Legend/>
+    <Tooltip/>
+  </LineChart>
+  </ResponsiveContainer>
+)
+}
